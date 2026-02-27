def2768d3804651f8bfe2fe83368e82e740f9dfb
JOOO <asdfaz12008@gmail.com>
2026-02-27 17:22:28 +0800

    chore: devagent auto checkpoint

---
+++ src/test.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Thiago
+ * @Date: 2026-02-27 17:21:55
+ * @LastEditors: Thiago
+ * @LastEditTime: 2026-02-27 17:22:00
+ * @FilePath: /devagent-mvp/src/test.js
+ * @Description:
+ */
+import { undefinedFunction } from "./src/test.js";
+console.log(undefinedFunction());
