aa1115d890b7373f2392b9ba666d7ae85d92d1ca
JOOO <asdfaz12008@gmail.com>
2026-03-01 14:04:40 +0800

    Delete src/test.js

--- src/test.js
+++
@@ -1,8 +0,0 @@
-import { test } from "node:test";
-import assert from "node:assert";
-import { add } from "../sandbox/mathUtils.js";
-
-test("add should return a + b", () => {
-  const result = add(2, 3);
-  assert.strictEqual(result, 5);
-});
